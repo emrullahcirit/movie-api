1242e22df0294f4d6b9c6220c76b93b3ec325b5e
Emrullah Cirit <emrullahcirit20@gmail.com>
2024-07-31 12:15:03 +0300

    test commit

--- responses/movie_response.go
+++ responses/movie_response.go
@@ -3,7 +3,7 @@ package responses
 import "github.com/gofiber/fiber/v2"
 
 type MovieResponse struct {
-	Status  int        `json:"status"`
-	Message string     `json:"message"`
-	Data    *fiber.Map `json:"data"`
+    Status  int        `json:"status"`
+    Message string     `json:"message"`
+    Data    *fiber.Map `json:"data"`
 }
